9f8c48373c4e80f7422c96bb0216f22166abcf91
parl <nopalnurhamid@gmail.com>
2021-10-06 19:53:05 +0700

    Kegiatan_PWD1
    Kegiatan prak PWD pertemuan 1

---
+++ latihan1_download.php
@@ -0,0 +1,9 @@
+<?php
+$myDir = "c:/xampp/htdocs/pwd1/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
